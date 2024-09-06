14a1f52fca7c03cf325fc3128d060a327bbcef1b
Okki Dwi <64502893+okkidwi@users.noreply.github.com>
2024-09-06 19:34:33 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -81,20 +81,22 @@ function init() {
           </div>
         </div>
       </div>
-<div class="col-lg-6 col-md-12">
-  <div class="card text-white mb-3 h-100">
-    <div class="card-header">
-      <i class="fa-regular fa-snowflake fa-fw"></i> Grup Official
-    </div>
-    <div class="card-body d-flex flex-wrap gap-1 justify-content-center align-items-center">
-      <a href="https://www.facebook.com/groups/153401064271807t" target="_blank" title="Yuk gabung sekarang!" style="margin-right: 3px;">
-        <img class="image" alt="Facebook" style="height: 33px;" src="https://raw.githubusercontent.com/okkidwi/okki-dwi-drive/master/images/facebook.png">
-      </a>
-      <a href="https://t.me/revancedindo" target="_blank" title="Yuk gabung sekarang!" style="margin-left: 3px;">
-        <img class="image" alt="Telegram" style="height: 33px;" src="https://raw.githubusercontent.com/okkidwi/okki-dwi-drive/master/images/telegram.png">
-      </a>
-    </div>
-  </div>
+			<div class="col-lg-6 col-md-12">
+			  <div class="card text-white mb-3 h-100">
+			    <div class="card-header">
+			      <i class="fa-regular fa-snowflake fa-fw"></i> Grup Official
+			    </div>
+			    <div class="card-body d-flex flex-wrap gap-1 justify-content-center align-items-center">
+			      <a href="https://www.facebook.com/groups/153401064271807t" target="_blank" title="Yuk gabung sekarang!" style="margin-right: 8px;">
+			        <img class="image" alt="Facebook" style="height: 33px;" src="https://raw.githubusercontent.com/okkidwi/okki-dwi-drive/master/images/facebook.png">
+			      </a>
+			      <a href="https://t.me/revancedindo" target="_blank" title="Yuk gabung sekarang!" style="margin-left: 8px;">
+			        <img class="image" alt="Telegram" style="height: 33px;" src="https://raw.githubusercontent.com/okkidwi/okki-dwi-drive/master/images/telegram.png">
+			      </a>
+            	</div>
+          	</div>
+        </div>
+    </div>	
 </div>
 
 <script>

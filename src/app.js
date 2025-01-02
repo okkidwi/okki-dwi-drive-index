5c5d499b532df4aedcc39ecd4eec7a23b042ad02
Okki Dwi <64502893+okkidwi@users.noreply.github.com>
2025-01-02 19:54:26 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -127,34 +127,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   </script>
 
-  <div class="row g-3 mt-0">
-  <div class="col-12">
-    <div class="card text-white mb-3 h-100">
-      <div class="card-header">
-        <i class="fa-regular fa-lightbulb"></i> Tentang Website
-      </div>
-      <div class="card-body">
-        <p class="card-text">Okki Dwi Drive adalah sebuah website direktori publik yang dimiliki oleh Okki Dwi. Anda dapat mengunduh berbagai aplikasi yang Anda anggap berguna. Semua berkas yang ada di website ini sudah melewati uji keamanan menggunakan Antivirus untuk menghindari dari ancaman virus yang dapat membahayakan perangkat Anda</p>
-      </div>
-    </div>
-	</div>
- </div>
-
-<style>
-  .card {
-    margin-top: 20px;
-    margin-bottom: 20px;
-    width: 100%;
-    height: auto;
-  }
-  .card-header {
-    padding: 10px 15px;
-  }
-  .card-body {
-    padding: 15px;
-  }
-</style>
-
 <div class="modal fade" id="SearchModel" data-bs-keyboard="true" tabindex="-1" aria-labelledby="SearchModelLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable" role="document">
     <div class="modal-content">
